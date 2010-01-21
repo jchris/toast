f3a1619cfda9a57027a9623f5efbf0f5963eefed
Chris Anderson <jchris@apache.org>
2010-01-20 23:25:12 -0800

    hello world event based login

--- vendor/couchapp/_attachments/jquery.couchapp.account.js
+++ vendor/couchapp/_attachments/jquery.couchapp.account.js
@@ -33,63 +33,72 @@ jQuery(function($) {
   };
 
   $.fn.couchappAccountWidget.base = {
-    // signUp and logIn are triggered by events set by loggedIn
-    signUp : [function(options) {
-
-    }],
-    logIn : [function(options) {
-      div.html('<form><label for="name">Name</label> <input type="text" name="name" value=""><label for="password">Password</label> <input type="password" name="password" value=""><input type="submit" value="Login"></form>');
-      $("input[name=name]", div).focus();
-      $('form', div).submit(function() {
-        var form = $(this);
-        $.couch.login({
-          name : $("input[name=name]", form).val(),
-          password : $("input[name=password]", form).val(),
-          success : refreshWidget
+    // signupForm and loginForm are triggered by events set by loggedOut
+    signupForm : [function(options) {
+      this.bind("org.couchapp.account.signupForm", function(e, selector) {
+        var div = $(this);
+        div.html('<form><label for="name">Name</label><input type="text" name="name" value=""><label for="password">Password</label><input type="password" name="password" value=""><input type="submit" value="Signup"></form>');
+        $("input[name=name]", div).focus();
+        $('form', div).submit(function() {
+          var form = $(this);
+          div.trigger("org.couchapp.account.doSignup", options, $("input[name=name]", form).val(), $("input[name=password]", form).val())
+          return false;
         });
-        return false;
+      });
+    }],
+    loginForm : [function(options) {
+      this.bind("org.couchapp.account.loginForm", function(e, selector) {
+        var widget = $(this);
+        widget.html('<form><label for="name">Name</label> <input type="text" name="name" value=""><label for="password">Password</label> <input type="password" name="password" value=""><input type="submit" value="Login"></form>');
+        $("input[name=name]", widget).focus();
+        $('form', widget).submit(function() {
+          var form = $(this);
+          $.couch.login({
+            name : $("input[name=name]", form).val(),
+            password : $("input[name=password]", form).val(),
+            success : function() {
+              widget.trigger("org.couchapp.account.refreshSession");
+            }
+          });
+          return false;
+        });
       });
     }],
     // loggedIn loggedOut and adminParty are triggered by the session xhr response
-    loggedIn : [function(options, userCtx) {
-      this.bind("org.couchapp.account.loggedIn", function(e, selector) {
+    loggedIn : [function(options) {
+      this.bind("org.couchapp.account.loggedIn", function(e, r) {
         // draw the welcome template
-        console.log("loggedIn")
+        var div = $(this);
+        
+        div.html('Welcome <a target="_new" href="/_utils/document.html?'+
+          encodeURIComponent(r.info.authentication_db) +
+          '/org.couchdb.user%3A' + 
+          encodeURIComponent(r.userCtx.name)+'">' +
+          r.userCtx.name +
+          '</a>! <a href="#logout">Logout?</a>');
+        // setup the logout callback
+        $('a[href=#logout]', div).click(function() {
+          $.couch.logout({
+            success : function() {
+              widget.trigger("org.couchapp.account.refreshSession");
+            }
+          });
+          return false;
+        });
       });
     }],
     loggedOut : [function(options, userCtx) {
       this.bind("org.couchapp.account.loggedOut", function(e, selector) {
-        // draw the loggedOut template
-        console.log("loggedOut")
-        $(this).html('<a href="#signup">Signup</a> or <a href="#login">Login</a>');
-        // setup login and signup callbacks
+        var widget = $(this);
+        widget.html('<a href="#signup">Signup</a> or <a href="#login">Login</a>');
          $('a[href=#login]', this).click(function() {
-           widget.trigger("org.couchapp.account.logIn", options, userCtx);
+           widget.trigger("org.couchapp.account.loginForm", userCtx);
            return false;
          });
-         
-         $('a[href=#signup]', div).click(function() {
-           div.html('<form><label for="name">Name</label><input type="text" name="name" value=""><label for="password">Password</label><input type="password" name="password" value=""><input type="submit" value="Signup"></form>');
-           $("input[name=name]", div).focus();
-           $('form', div).submit(function() {
-             var form = $(this);
-             var name = $("input[name=name]", form).val();
-             var password = $("input[name=password]", form).val();
-             $.couch.signup({
-               name : name
-             }, password, {
-               success : function() {
-                 $.couch.login({
-                   name : name,
-                   password : password,
-                   success : refreshWidget
-                 });
-               }
-             });
-             return false;
-           });
-
-
+         $('a[href=#signup]', this).click(function() {
+           widget.trigger("org.couchapp.account.signupForm", userCtx);
+           return false;
+         });
       });
     }],
     adminParty : [function(options, userCtx) {
@@ -105,12 +114,29 @@ jQuery(function($) {
           success : function(r) {
             var userCtx = r.userCtx;
             if (userCtx.name) {
-              widget.trigger("org.couchapp.account.loggedIn", options, userCtx);
+              widget.trigger("org.couchapp.account.loggedIn", r);
             } else if (userCtx.roles.indexOf("_admin") != -1) {
-              widget.trigger("org.couchapp.account.adminParty", userCtx);
+              widget.trigger("org.couchapp.account.adminParty", r);
             } else {
-              widget.trigger("org.couchapp.account.loggedOut", userCtx);
+              widget.trigger("org.couchapp.account.loggedOut", r);
             };
+          }
+        });
+      });
+    }],
+    doSignup : [function(options) {
+      this.bind("org.couchapp.account.doSignup", function(e, name, pass) {
+        $.couch.signup({
+          name : name
+        }, pass, {
+          success : function() {
+            $.couch.login({
+              name : name,
+              password : pass,
+              success : function() {
+                widget.trigger("org.couchapp.account.refreshSession");
+              }
+            });
           }
         });
       });
