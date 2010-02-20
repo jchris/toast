f2a9e6b02d6a8a6cc4d0717841accab8d4555638
Chris Anderson <jchris@apache.org>
2010-02-19 21:08:23 -0500

    remove some old markup

--- vendor/couchapp/_attachments/jquery.couch.app.util.js
+++ vendor/couchapp/_attachments/jquery.couch.app.util.js
@@ -11,12 +11,13 @@ function safeHTML(st, len) {
   return st ? escapeHTML(st.substring(0,len)) : '';
 }
 
-function linkify(body, term) {
-  return body.replace(/https?\:\/\/\S+/g,function(a) {
+// todo this should take a replacement template
+$.linkify = function(body) {
+  return body.replace(/((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/gi,function(a) {
     return '<a target="_blank" href="'+a+'">'+a+'</a>';
   }).replace(/\@([\w\-]+)/g,function(user,name) {
-    return '<a target="_blank" href="http://twitter.com/'+name+'">'+user+'</a>';
-  }).replace(/\#([\w\-]+)/g,function(word,term) {
-    return '<a href="#'+encodeURIComponent(term)+'">'+word+'</a>';
+    return '<a href="#/mentions/'+encodeURIComponent(name)+'">'+user+'</a>';
+  }).replace(/\#([\w\-\.]+)/g,function(word,tag) {
+    return '<a href="#/tags/'+encodeURIComponent(tag)+'">'+word+'</a>';
   });
 };
