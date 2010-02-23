50e9b071825f4cff47400cdc03960c7586115d6c
Chris Anderson <jchris@apache.org>
2010-02-23 12:02:33 -0500

    style the chats

--- evently/chat/join/data.js
+++
@@ -1,7 +0,0 @@
-function(resp) {
-  var p = $$("#profile").profile;
-  $.log(p)
-  var v =  resp.rows[0].value;
-  v.profile = p;
-  return p;
-};
--- evently/chat/join/selectors/ul/_changes/data.js
+++ evently/chat/join/selectors/ul/_changes/data.js
@@ -3,8 +3,8 @@ function(row) {
   return {
     avatar_url : v.author && v.author.gravatar_url,
     body : $.linkify($.mustache.escape(v.body)),
-    name : v.author && v.author.name,
-    name_uri : v.author && encodeURIComponent(v.author.name),
+    name : v.author && v.author.nickname,
+    url : v.author && v.author.url,
     created_at : $.prettyDate(v.created_at)
   };
 };
