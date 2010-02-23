942c51becd36cc88714d4f5865b54231ca5c87b4
Chris Anderson <jchris@apache.org>
2010-02-23 11:40:29 -0500

    profile is the input form

--- evently/chat/join/after.js
+++ evently/chat/join/after.js
@@ -1,5 +1,6 @@
 function(resp) {
   var chan = resp.rows[0].value;
+  $$(this).channel = chan.channel;
   $("h1").text("Toast :: "+chan.channel);
   $("#chandesc").text(chan.desc);
 };
--- evently/profile/profileReady/selectors/form/submit.js
+++ evently/profile/profileReady/selectors/form/submit.js
@@ -1,9 +1,8 @@
 function(e) {
   var f = $(this);
-  var chan = e.data.args[0].rows[0].value;
   var doc = {
     body : $("input[name=message]", f).val(),
-    channel : chan.channel,
+    channel : $$("#chat").channel,
     author : $$("#profile").profile,
     created_at : new Date()
   };
